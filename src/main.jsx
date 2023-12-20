4bb0530a7ad3192a448b0ff64361ff976eb84d3c
Numan Ali <numan4599322ali@gmail.com>
2023-12-20 17:30:41 +0530

    final

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,8 @@ import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-{/* <GoogleOAuthProvider clientId="694462394853-b484l6of03vg6114t4ng1rm9186m03po.apps.googleusercontent.com"> */}
-<GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+<GoogleOAuthProvider clientId="694462394853-b484l6of03vg6114t4ng1rm9186m03po.apps.googleusercontent.com">
+{/* <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}> */}
 <Provider store={store}>
     <App />
   </Provider>
