33b0a7b3a90c6792dc6765f45d8c9080595a796b
Numan Ali <numan4599322ali@gmail.com>
2023-12-20 18:08:39 +0530

    final

--- src/api.js
+++ src/api.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-export const fetchApi = (url)=> axios.get(`http://localhost:5000${url}`)
-export const postApi = (url,data)=> axios.post(`http://localhost:5000${url}`,data)
-export const patchApi = (url,data)=> axios.patch(`http://localhost:5000${url}`,data)
-export const deleteApi = (url)=> axios.delete(`http://localhost:5000${url}`)
+// export const fetchApi = (url)=> axios.get(`http://localhost:5000${url}`)
+// export const postApi = (url,data)=> axios.post(`http://localhost:5000${url}`,data)
+// export const patchApi = (url,data)=> axios.patch(`http://localhost:5000${url}`,data)
+// export const deleteApi = (url)=> axios.delete(`http://localhost:5000${url}`)
+
+
+export const fetchApi = (url)=> axios.get(`https://linkedin-clone-back.onrender.com${url}`)
+export const postApi = (url,data)=> axios.post(`https://linkedin-clone-back.onrender.com${url}`,data)
+export const patchApi = (url,data)=> axios.patch(`https://linkedin-clone-back.onrender.com${url}`,data)
+export const deleteApi = (url)=> axios.delete(`https://linkedin-clone-back.onrender.com${url}`)
