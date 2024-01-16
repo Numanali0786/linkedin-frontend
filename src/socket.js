f7357ef81377264770c6fe681508917babba891c
Numan Ali <numan4599322ali@gmail.com>
2024-01-16 12:19:15 +0530

    msg

--- src/socket.js
+++ src/socket.js
@@ -3,9 +3,12 @@ import io from 'socket.io-client';
 var socket;
 
 const connectSocket = (user_id)=>{
-    socket = io("http://localhost:5000",{
+    socket = io("https://linkedin-clone-back.onrender.com",{
         query:`user_id=${user_id}`
     })
+    // socket = io("http://localhost:5000",{
+    //     query:`user_id=${user_id}`
+    // })
     // console.log(socket)
 }
 
